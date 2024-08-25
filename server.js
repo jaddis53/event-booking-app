187cb07332d9b872acf4e0873d4485a261a62dfb
Jalal Addisu <jayaddisu@gmail.com>
2024-08-25 18:58:41 +0300

    defined the endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const bodyParser=require('body-parser')
+const graphqlHttp=require('express-graphql')
 
 const app=express()
 app.use(bodyParser.urlencoded({extended:false}))
@@ -9,9 +10,7 @@ app.listen(3000,()=>{
     console.log('server is working just fine')
 })  
 
-app.get('/get',(req,res,next)=>{
-
-    res.send('this is working fimnxjne as well')
-    next()
-    
-})
+app.use('/graphql',graphqlHttp({
+    schema:null,
+    rootValue:{}
+}))
